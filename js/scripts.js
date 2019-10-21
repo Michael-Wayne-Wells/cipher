4fa112cc68fd4bb01464983ce29f9ecba9851c66
Student <example@example.com>
2019-10-21 15:35:09 -0700

    replace first with last and last with first.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 var sentence = prompt("Enter a sentence");
  var newSentence = function(sentence) {
    var upperCaseLast = sentence.slice(0, -1) + sentence.charAt(sentence.length-1).toUpperCase();
+
    var upperCaseFirst = upperCaseLast.charAt(sentence.length[0]).toUpperCase() + upperCaseLast.slice(1);
 
-   return upperCaseFirst;
- };
- // var upperCaseFirst = function(sentence){
- //    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
- // };
+    var firstLetter = upperCaseFirst[0];
+    var reverseFirst = upperCaseFirst.replace(new RegExp('^' + upperCaseFirst[0]), upperCaseFirst[upperCaseFirst.length-1]);
 
-//
-// console.log(upperCaseFirst(sentence));
- alert(newSentence(sentence));
+    var reverseLast = reverseFirst.replace(new RegExp(reverseFirst[reverseFirst.length-1] + '$'), firstLetter);
+
+   return reverseLast;
+   alert(reverseSentence);
+ };
+// var reverseSentence = function(upperCaseFirst){
+alert(newSentence(sentence));
